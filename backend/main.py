9acd028e990ce40d751fc8e366495cf927a190cb
Emmanuel-technolite <30640696@students.lincoln.ac.uk>
2026-05-19 17:23:20 +0100

    Refactor import statements in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException,
 from fastapi.staticfiles import StaticFiles
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
